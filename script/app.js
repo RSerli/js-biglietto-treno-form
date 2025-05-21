f50ac3afc6dc56adbfab4cc4e3d5c5c4895aee7f
Rserli <riccardoserli.95@gmail.com>
2025-05-21 22:04:00 +0200

    Begin JS

---
+++ script/app.js
@@ -0,0 +1,19 @@
+/*
+* --- VARIABILI ---
+*/
+const testoLunghezzaTratta = document.getElementById('testoLunghezzzaTratta')
+console.dir(testoLunghezzaTratta)
+const selezioneSconto = document.getElementById('selezioneSconto')
+console.dir(selezioneSconto)
+const bottoneCalcolo = document.getElementById('bottoneCalcolaPrezzo')
+console.dir(bottoneCalcolo)
+const RigaPrezzoFinale = document.getElementById('PrezzoFinaleBiglietto')
+console.dir(RigaPrezzoFinale)
+
+const COSTO_KILOMETRAGGIO = 0.21
+const SCONTO_UNDER18 = 20
+const SCONTO_OVER65 = 40
+
+/*
+* --- FUNZIONI ---
+*/
